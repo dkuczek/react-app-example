64a5936f7528830d827e891e9c8076b59ad9f282
Damian <damian.kuczek15@gmail.com>
2024-11-13 08:07:17 +0100

    Updates

---
+++ precache-manifest.023babb0f7e04716d98879556dbdc0f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e88875b73e3f76b7f02",
+    "url": "/react-app-example/static/css/main.7950070b.chunk.css"
+  },
+  {
+    "revision": "7e88875b73e3f76b7f02",
+    "url": "/react-app-example/static/js/main.cca4d659.chunk.js"
+  },
+  {
+    "revision": "add9467de19790d38ebc",
+    "url": "/react-app-example/static/js/runtime~main.2ed37660.js"
+  },
+  {
+    "revision": "1e3ebd9cb366b19fa0f2",
+    "url": "/react-app-example/static/js/2.ec4eadad.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-example/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0096b1f5519db22e5bab2570b0cd7e39",
+    "url": "/react-app-example/index.html"
+  }
+];
